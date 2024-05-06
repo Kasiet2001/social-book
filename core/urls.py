5e0780d5111267edceec46b830e464204e18b610
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-05-06 20:55:52 +0600

    signin and logout

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup')
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
-
+@login_required(login_url='core:signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -41,3 +42,25 @@ def signup(request):
 
     else:
         return render(request, 'signup.html')
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('core:signin')
+    else:
+        return render(request, 'signin.html')
+
+@login_required(login_url='core:signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('core:signin')
