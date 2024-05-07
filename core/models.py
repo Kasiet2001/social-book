6cc601729902c3f35bd1b373bda15ab293195c1e
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-05-07 19:44:40 +0600

    account settings

--- core/models.py
+++ core/models.py
@@ -7,9 +7,9 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
-    bio = models.TextField(blank=True, null=True)
+    bio = models.TextField(blank=True)
     profile_img = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
-    location = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
